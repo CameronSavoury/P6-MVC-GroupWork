46e3397e6d3a465df9785d7ced9119085a39e575
CameronSavoury <31745632+CameronSavoury@users.noreply.github.com>
2021-02-16 16:27:23 +0000

    Create Multiple Instances of Controller 2
    Two new deceleration's of Controller 2 with appropriate variable names to distinguish between them. Model variable 'model' is passed in as a parameter to satisfy the requirements of the Controller2 Constructor.

--- Main.java
+++ Main.java
@@ -12,7 +12,11 @@ public class Main {
      
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model); // Create first controller
+        
+        //Instantiate Multiple Instances of Controller 2
         Controller2 c2 = new Controller2(model); // Create second controller
+        Controller2 c21 = new Controller2(model); // Create 2nd instance of the second controller
+        Controller2 c22 = new Controller2(model); // Create 3rd instance of thesecond controller
         
     } // main
   
