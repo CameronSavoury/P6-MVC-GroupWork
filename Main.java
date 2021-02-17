f7579312dda635afb22534bb6fd29fe843f94755
Nathan <Nathan@DESKTOP-UHODC3M>
2021-02-17 12:44:00 +0000

    Added three Parameters too Controller one and two's constructor.
    Updated set title and set position in controller one and two.
    
    Updated main to pass all parameters needed.

--- Main.java
+++ Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
+        Controller1 c1 = new Controller1(model, "Controller 1 (Main)", 40, 40); // Create first controller
+        Controller2 c2 = new Controller2(model, "Observer 1", 380, 40); // Create second controller
         
     } // main
   
