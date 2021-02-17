106bb2146c07337c1b4353632258aedaac31bdff
Nathan <Nathan@DESKTOP-UHODC3M>
2021-02-17 13:57:29 +0000

    Added two controller twos named c21 & c22 along with paramaters

--- Main.java
+++ Main.java
@@ -13,7 +13,9 @@ public class Main {
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model, "Controller 1 (Main)", 40, 40); // Create first controller
         Controller2 c2 = new Controller2(model, "Observer 1", 380, 40); // Create second controller
+        Controller2 c21 = new Controller2(model, "Observer 2", 840, 40);
+        Controller2 c22 = new Controller2(model, "Observer 3", 1200, 40);
         
-    } // main
+    } // main1
   
 } // Main
